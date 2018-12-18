8e20f7351d8e8bb127362a23c88dc688b5017d29
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2018-12-18 17:45:29 -0200

    Evento Repository

--- src/main/java/br/com/eventosapp/controllers/EventoController.java
+++ src/main/java/br/com/eventosapp/controllers/EventoController.java
@@ -1,13 +1,29 @@
 package br.com.eventosapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.eventosapp.models.Evento;
+import br.com.eventosapp.repository.EventoRepository;
 
 @Controller 
 public class EventoController {
 	
-	@RequestMapping("/cadastrar-evento")
+	@Autowired
+	private EventoRepository eventoRepository;
+	
+	@RequestMapping(value="/cadastrar-evento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastrar-evento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		
+		eventoRepository.save(evento);
+		
+		return "redirect:/cadastrar-evento";
 	}
 }
---
+++ src/main/java/br/com/eventosapp/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package br.com.eventosapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.eventosapp.models.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, String> {
+	
+}
