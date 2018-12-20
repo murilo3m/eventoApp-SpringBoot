22059cab8cc3cd220a0c94962cd0005ae048daa2
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2018-12-20 15:53:53 -0200

    Eventos List / Materialize css

--- src/main/java/br/com/eventosapp/controllers/EventoController.java
+++ src/main/java/br/com/eventosapp/controllers/EventoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.eventosapp.models.Evento;
 import br.com.eventosapp.repository.EventoRepository;
@@ -25,5 +26,13 @@ public class EventoController {
 		eventoRepository.save(evento);
 		
 		return "redirect:/cadastrar-evento";
+	}
+	
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = eventoRepository.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
